9e93004ea1a0c3064383b789af898926df9bd5f4
Kyle Metivier <metivier.kyle@yahoo.com>
2025-01-16 14:40:40 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,8 @@ use App\Http\Controllers\PhotoController;
 use App\Models\Photo;
 
 // Show the photo upload form (assuming you have a controller for this route)
-Route::get('/{session?}', [PhotoController::class, 'showForm']);
+Route::get('/', [PhotoController::class, 'showForm']);
+Route::get('mobile/{session}', [PhotoController::class, 'showForm']);
 
 // Handle photo upload
 Route::post('/upload', [PhotoController::class, 'upload']);
