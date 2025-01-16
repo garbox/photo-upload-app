12119c98670b7c4ded6441b707b44759616e8eaf
Kyle Metivier <metivier.kyle@yahoo.com>
2025-01-16 14:38:07 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,20 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PhotoController;
 use App\Models\Photo;
 
-Route::get('home/{session?}', [PhotoController::class, 'showForm']);
+// Show the photo upload form (assuming you have a controller for this route)
+Route::get('/{session?}', [PhotoController::class, 'showForm']);
+
+// Handle photo upload
 Route::post('/upload', [PhotoController::class, 'upload']);
+
+// Fetch photos based on session ID and return them as JSON
 Route::get('/photos', function() {
+    // Get photos by session ID
     $photos = Photo::where('session_id', session()->getId())->get();
-    dump($photos);
-    return json($photos);
+    
+    // If you want to debug, use `dd()` instead of `dump()` to inspect the photos
+    // dd($photos);
+    
+    // Return the photos as a JSON response
+    return response()->json($photos);
 });
