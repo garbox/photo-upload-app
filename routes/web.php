44dd08977871acecca80d4497527c0e094802260
Kyle Metivier <metivier.kyle@yahoo.com>
2025-01-16 14:35:52 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PhotoController;
 use App\Models\Photo;
 
-Route::get('/{session?}', [PhotoController::class, 'showForm']);
+Route::get('home/{session?}', [PhotoController::class, 'showForm']);
 Route::post('/upload', [PhotoController::class, 'upload']);
 Route::get('/photos', function() {
     $photos = Photo::where('session_id', session()->getId())->get();
