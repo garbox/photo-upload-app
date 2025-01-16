3f25b37148f9a9944a864c07311c04d1fde69ab7
Kyle Metivier <metivier.kyle@yahoo.com>
2025-01-16 16:16:34 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,7 @@ Route::post('/upload', [PhotoController::class, 'upload']);
 Route::get('/photos', function() {
     // Get photos by session ID
     $photos = Photo::where('session_id', session()->getId())->get();
-    
-    // If you want to debug, use `dd()` instead of `dump()` to inspect the photos
-    // dd($photos);
-    
+        
     // Return the photos as a JSON response
     return response()->json($photos);
 });
