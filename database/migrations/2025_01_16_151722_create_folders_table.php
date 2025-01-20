aa006832ec3677ff2d588fa8e5c83e6d56165235
Kyle Metivier <metivier.kyle@yahoo.com>
2025-01-20 15:06:36 -0600

    Create Folders migration and model

---
+++ database/migrations/2025_01_16_151722_create_folders_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\User;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('folders', function (Blueprint $table) {
+            $table->id();
+            $table->string('folder_name');
+            $table->string('folder_description');
+            $table->foreignIdFor(User::class)->refrecnes('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('folders');
+    }
+};
